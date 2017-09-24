7411f1f1137188850abfdfd4dfa0a9545dccf504
641753527@qq.com <641753527@qq.com>
2017-09-24 17:21:31 +0800

    21

--- api/seller.php
+++ api/seller.php
@@ -11,7 +11,7 @@ $news = array(
   
 header('content-type:application:json;charset=utf8');
 // 指定允许其他域名访问  
-header('Access-Control-Allow-Origin:*');  
+header('Access-Control-Allow-Origin:https://f641753527.github.io');  
 // 响应类型  
 header('Access-Control-Allow-Methods:GET');  
 // 响应头设置  
